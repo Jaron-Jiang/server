baf8bf35acba2fa6a0e5ed70f7c0ace02e252757
fujiaxu <858813020@qq.com>
2020-11-22 00:41:18 +0800

    添加SignInHistory 和 SignInHistoryRecord 类

---
+++ SignInServicerClass/SignInHistory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cn.swu_acm.projects.sia.libs
+{
+    class SignInHistory : Info
+    {
+        private string sign_in_history_starttime;
+        private string sign_in_history_endtime;
+        private LinkedList<SignInHistoryRecord> sign_in_history_data;
+        
+        public string SignInHistoryStarttime
+        {
+            get => sign_in_history_starttime;
+            set
+            {
+                sign_in_history_starttime = value;
+            }
+        }
+        public string SignInHistoryEndtime
+        {
+            get => sign_in_history_endtime;
+            set
+            {
+                sign_in_history_endtime = value;
+            }
+        }
+        public LinkedList<SignInHistoryRecord> SignHistoryData
+        {
+            get => sign_in_history_data;
+            set
+            {
+                sign_in_history_data = value;
+            }
+        }
+        public void AddHistoryRecord(SignInHistoryRecord record)
+        {
+            SignHistoryData.AddLast(record);
+        }
+        public SignInHistory()
+        {
+            SignInHistoryStarttime = "19700101000000";
+            SignInHistoryEndtime = "19700101000000";
+            SignHistoryData = new LinkedList<SignInHistoryRecord>();
+        }
+    }
+}
---
+++ SignInServicerClass/SignInHistoryRecord.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cn.swu_acm.projects.sia.libs
+{
+    public enum SignIn_Type
+    {
+        Nothing,
+        Location,
+        Audio,
+        Face,
+        List
+    }
+    class SignInHistoryRecord : Info
+    {
+        private string sign_in_complete_Datetime;
+        private string sign_in_type;
+        private string sign_in_starttime;
+        private string sign_in_endtime;
+        public string SignInCompleteDatetime
+        {
+            get => sign_in_complete_Datetime;
+            set
+            {
+                sign_in_complete_Datetime = value;
+            }
+        }
+        public string SignInType
+        {
+            get => sign_in_type;
+            set
+            {
+                sign_in_type = value;
+            }
+        }
+        public string SignInStartTime
+        {
+            get => sign_in_starttime;
+            set
+            {
+                sign_in_starttime = value;
+            }
+        }
+        public string SignInEndtime
+        {
+            get => sign_in_endtime;
+            set
+            {
+                sign_in_endtime = value;
+            }
+        }
+        public SignInHistoryRecord()
+        {
+            SignInCompleteDatetime = "19700101000000";
+            SignInType = SignIn_Type.Nothing.ToString();
+            SignInStartTime = "19700101000000";
+            SignInEndtime = "19700101000000";
+        }
+
+    }
+}
