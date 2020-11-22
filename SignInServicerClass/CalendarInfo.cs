d40ebeecac3d0a468cbdb8f17f3035f546148c8b
fujiaxu <57003959+fujiaxu0207@users.noreply.github.com>
2020-11-22 12:57:29 +0800

    jiaxu add the CalendarInfo.cs
    jiaxu add the CalendarInfo.cs

---
+++ SignInServicerClass/CalendarInfo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cn.swu_acm.projects.sia.libs
+{
+    class CalendarInfo:Info
+    {
+        private string calendar_title;
+        private string calendar_starttime;
+        private string calendar_endtime;
+        private string calendar_address;
+        private string calendar_description;
+        public string CalendarTitle
+        {
+            get => calendar_title;
+            set
+            {
+                calendar_title = value;
+            }
+        }
+        public string CalendarStarttime
+        {
+            get => calendar_starttime;
+            set
+            {
+                calendar_starttime = value;
+            }
+        }
+        public string CalendarEndtime
+        {
+            get => calendar_endtime;
+            set
+            {
+                calendar_endtime = value;
+            }
+        }
+        public string CalendarAddress
+        {
+            get => calendar_address;
+            set
+            {
+                calendar_address = value;
+            }
+        }
+        public string CalendarDescription
+        {
+            get => calendar_description;
+            set
+            {
+                calendar_description = value;
+            }
+        }
+        public CalendarInfo()
+        {
+            CalendarTitle = "Unknown";
+            CalendarStarttime = "19700101000000";
+            CalendarEndtime = "19700101000000";
+            CalendarAddress = "Unknown";
+            CalendarDescription = "";
+        }
+    }
+}
