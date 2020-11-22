37cc2b797e520e79264c34f6a9037647512a2472
fujiaxu <57003959+fujiaxu0207@users.noreply.github.com>
2020-11-22 12:47:41 +0800

    jiaxu add UserInfo.cs and UserInfoList.cs
    jiaxu add UserInfo.cs and UserInfoList.cs

---
+++ SignInServicerClass/UserInfoList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cn.swu_acm.projects.sia.libs
+{
+    class UserInfoList:Info
+    {
+        private LinkedList<UserInfo> users;
+        public LinkedList<UserInfo> Users
+        {
+            get => users;
+            set
+            {
+                users = value;
+            }
+        }
+        public void Add(UserInfo user_info)
+        {
+            Users.AddLast(user_info);
+        }
+        public UserInfoList()
+        {
+            Users = new LinkedList<UserInfo>();
+        }
+    }
+}
