a9de0aa3dff4ae83bba7855b278c28b38cc1e07f
fujiaxu <57003959+fujiaxu0207@users.noreply.github.com>
2020-11-22 12:19:09 +0800

    Add files via upload
    jiaxu add the MessageInfi.cs and MessageList.cs

---
+++ SignInServicerClass/MessageInfo.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cn.swu_acm.projects.sia.libs
+{
+    class MessageInfo:Info
+    {
+        private string message_info_id;
+        private string message_info_publisher;
+        private string message_info_type;
+        private string message_info_title;
+        private string message_info_descripiton;
+        private string message_info_starttime;
+        private LinkedList<UserSet> accepter;
+
+        public string MessageInfoId
+        {
+            get => message_info_id;
+            set
+            {
+                message_info_id = value;
+            }
+        }
+        public string MessageInfoPublisher
+        {
+            get => message_info_publisher;
+            set
+            {
+                message_info_publisher = value;
+            }
+        }
+        public string MessageInfoType
+        {
+            get => message_info_type;
+            set
+            {
+                message_info_type = value;
+            }
+        }
+        public string MessageInfoTitle
+        {
+            get => message_info_title;
+            set
+            {
+                message_info_title = value;
+            }
+        }
+        public  string MessageInfoDescripiton
+        {
+            get => message_info_descripiton;
+            set
+            {
+                message_info_descripiton = value;
+            }
+        }
+        public string MessageInfoStarttime
+        {
+            get => message_info_starttime;
+            set
+            {
+                message_info_starttime = value;
+            }
+        }
+        public LinkedList<UserSet> Accepter
+        {
+            get => accepter;
+            set
+            {
+                accepter = value;
+            }
+        }
+        public MessageInfo()
+        {
+            MessageInfoId = "";
+            MessageInfoPublisher = "";
+            MessageInfoType = "";
+            MessageInfoTitle = "";
+            MessageInfoDescripiton = "";
+            MessageInfoStarttime = "19700101000000";
+            Accepter = new LinkedList<UserSet>();
+        }
+    }
+}
---
+++ SignInServicerClass/MessageList.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cn.swu_acm.projects.sia.libs
+{
+    class MessageList:Info
+    {
+        private string message_starttime;
+        private string message_endtime;
+        private LinkedList<MessageInfo> message_list_data;
+        public string MessageStarttime
+        {
+            get => message_starttime;
+            set
+            {
+                message_starttime = value;
+            }
+        }
+        public string MessageEndtime
+        {
+            get => message_endtime;
+            set
+            {
+                message_endtime = value;
+            }
+        }
+        public LinkedList<MessageInfo> MessageListData
+        {
+            get => message_list_data;
+            set
+            {
+                message_list_data = value;
+            }
+        }
+
+        public void AddHistory(MessageInfo message_info)
+        {
+            MessageListData.AddLast(message_info);
+        }
+        public MessageList()
+        {
+            MessageStarttime = "19700101000000";
+            MessageEndtime = "19700101000000";
+            MessageListData = new LinkedList<MessageInfo>();
+        }
+    }
+}
